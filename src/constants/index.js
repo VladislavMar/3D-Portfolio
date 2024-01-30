ef84aa2cd00077ff7c26310dca9548b351c5585a
Vlad <vladm19082006@gmail.com>
2024-01-30 21:46:42 +0300

    Hero section done

--- src/constants/index.js
+++ src/constants/index.js
@@ -46,15 +46,15 @@ import {
       icon: web,
     },
     {
-      title: "React Native Developer",
+      title: "React Developer",
       icon: mobile,
     },
     {
-      title: "Backend Developer",
+      title: "Angular Developer",
       icon: backend,
     },
     {
-      title: "Content Creator",
+      title: "Node.js Developer",
       icon: creator,
     },
   ];
